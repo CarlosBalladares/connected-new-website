be544fd7587315edd83e86f6d81effc651675199
Carlos Balladares <carlos@balladares.co>
2018-07-20 17:13:23 -0700

    bringing in mock data from live branch

---
+++ src/assets/Releases.js
@@ -0,0 +1,69 @@
+import Release1  from './img/Echo.jpg'
+import Release1Blur  from './img/EchoBlur.jpg'
+import Release2  from './img/Anility - State Of Mind.jpg'
+import Release3  from './img/906 - Lunar.jpg'
+import Release4  from './img/PYTHR - Terror VIP.jpg'
+import Release5  from './img/The Fallen Night - Anger.jpg'
+
+
+const Releases =[
+    
+    {
+        title: 'Echo',
+        artist: 'Pythr',
+        message: 'Pythr is back and has a brand new song for yall to hear!!! Something big is coming soon from pythr!!!',
+        soundcloud: 'pythrbeats',
+        scEmbed: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/469143129&color=%235c5c5c&inverse=true&auto_play=false&show_user=true',
+        youtube: 'pythrbeats',
+        twitter:  'pythrmusic',
+        instagram: 'pythrmusic',
+        artwork: Release1,
+        artworkBlur: Release1Blur,
+        latest: 'true'
+      },{
+        title: 'State Of Mind',
+        artist: 'Anility',
+        message: 'Anility has a new state of mind in todays track! Welcome back Anility!!!',
+        soundcloud: 'anility',
+        scEmbed: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/467800014&color=%23636368&inverse=true&auto_play=false&show_user=true',
+        youtube: 'AnilityMusic',
+        twitter:  'AnilityMusic',
+        instagram: 'AnilityMusic',
+        artwork: Release2,
+      },{
+        title: '906',
+        artist: 'Lunar',
+        message: 'The Fallen Night seems a little salty after his Anger takes control of him!',
+        soundcloud: '9-0-6',
+        scEmbed: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/463971618&color=%23636368&inverse=true&auto_play=false&show_user=true',
+        youtube: '9-0-6',
+        twitter:  '9-0-6',
+        instagram: '9-0-6',
+        artwork: Release3,
+      },{
+        title: 'Terror (VIP)',
+        artist: 'Pythr',
+        message: 'Pythr is back and has a VIP of his original song... Terror... It\'s kinda terrifying how hes only released terror with us.',
+        soundcloud: 'pythrbeats',
+        scEmbed: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/460288026&color=%23636368&inverse=true&auto_play=false&show_user=true',
+        youtube: 'pythrbeats',
+        twitter:  'pythrmusic',
+        instagram: 'pythrmusic',
+        artwork: Release4,
+      },{
+        title: 'Anger',
+        artist: 'The Fallen Night',
+        message: 'Pythr is back and has a VIP of his original song... Terror... It\'s kinda terrifying how hes only released terror with us.',
+        soundcloud: 'thefallennight',
+        scEmbed: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/457177467&color=%23636368&inverse=true&auto_play=false&show_user=true',
+        youtube: 'thefallennight',
+        twitter:  'thefallennight',
+        instagram: 'thefallennight',
+        artwork: Release5,
+      },
+          
+          
+    
+]
+
+export default Releases;
